ba768c7d91dd9037ce19309fc4d58659a6494420
SeunPaul <seunpaul148@gmail.com>
2022-12-24 09:34:05 +0100

    index page

--- index.php
+++ index.php
@@ -1,30 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        .container {
-            width: 100%;
-        }
-
-        .inner-container {
-            text-align: center;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="container">
-        <div class="inner-container">
-            <p>Who are you ?</p>
-            <button onclick="document.location.href='./admin-dashboard.php'"> ADMIN </button> <br><br>
-            <button onclick="document.location.href='./login.php'"> PATIENT </button>
-        </div>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <title>SMIFF BLOOD DONATION</title>
+  </head>
+  <body class="register-page">
+    <div class="register-left">
+      <img class="sand-background" src="images/sand.png" />
+      <img class="logo" src="images/logo-white.png" />
+      <img
+        class="register-illustration"
+        src="images/register-illustration.png"
+      />
+      <img class="register-image" src="images/register-image.png" />
     </div>
-</body>
-
+    <div class="register-right">
+      <div class="entry-btns">
+        <button
+          onclick="document.location.href='admin-dashboard.php'"
+          type="button"
+        >
+          ADMIN
+        </button>
+        <button onclick="document.location.href='login.php'" type="button">
+          PATIENT
+        </button>
+      </div>
+    </div>
+  </body>
 </html>
